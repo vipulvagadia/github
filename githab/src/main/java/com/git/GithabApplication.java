0c483399eb6d2482c1651a5143f9e5d5a964ba50
ZeronSec <ZeronSec@DESKTOP-BCF229R>
2021-03-27 14:55:34 +0530

    comit all file

---
+++ githab/src/main/java/com/git/GithabApplication.java
@@ -0,0 +1,15 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithabApplication.class, args);
+		
+		System.out.println("git hub");
+	}
+
+}
